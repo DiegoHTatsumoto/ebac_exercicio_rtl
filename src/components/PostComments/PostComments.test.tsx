132abe3b83f11c86382a4fd1c24b57042b648ef6
Diego <diego.trabalhodev@gmail.com>
2026-02-22 17:19:09 -0300

    teste de inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,25 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    it('teste para a inserção de dois comentários', () => {
+        render(<PostComment/>)
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'primeiro comentário',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'segundo comentário',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getAllByTestId('comment-li')).toHaveLength(2);
+        expect(screen.getByText('primeiro comentário')).toBeInTheDocument();
+        expect(screen.getByText('segundo comentário')).toBeInTheDocument();
+    })
 });
+
